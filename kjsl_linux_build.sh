397bd7fdc1e6ff7ff08800ad85010fae3dee4f4e
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2022-08-23 12:27:24 +1000

    KJSL builders

---
+++ kjsl_linux_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev
+
+mkdir -p build
+cd build || exit 1
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j$(nproc)
+sudo make install
---
+++ kjsl_macos_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p build
+cd build || exit 1
+
+cmake -D CMAKE_BUILD_TYPE=DEBUG \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j$(nproc)
+sudo make install
