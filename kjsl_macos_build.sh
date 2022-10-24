b51d97566c1d16c4c589006e2b261c7f6fda5b1a
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2022-10-25 09:23:22 +1100

    SLURPY-7 Docker (CUDA + OpenCL bare metal GCC)

--- kjsl_macos_build.sh
+++ kjsl_macos_build.sh
@@ -11,8 +11,9 @@ cmake -D CMAKE_BUILD_TYPE=DEBUG \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_GENERATE_PKGCONFIG=ON \
     -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-    -D BUILD_EXAMPLES=ON .. \
-    -D HAVE_FFMPEG=ON
+    -D BUILD_EXAMPLES=ON \
+    -D WITH_FFMPEG=ON \
+    -D WITH_OPENCL=ON
 
 make -j$(nproc)
 sudo make install
