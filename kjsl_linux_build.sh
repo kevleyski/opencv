b8a1d9cd70c48e537de71179094a1684c14edaa3
Kevin Staunton-Lambert <kevstal@fb.com>
2022-11-04 10:39:14 +1100

    KJSL build

--- kjsl_linux_build.sh
+++ kjsl_linux_build.sh
@@ -15,7 +15,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_GENERATE_PKGCONFIG=ON \
     -D WITH_FFMPEG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+    -D WITH_OPENCL=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
     -D BUILD_EXAMPLES=ON ..
 
 make -j$(nproc)
--- kjsl_macos_build.sh
+++ kjsl_macos_build.sh
@@ -12,7 +12,8 @@ cmake -D CMAKE_BUILD_TYPE=DEBUG \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_GENERATE_PKGCONFIG=ON \
     -D WITH_FFMPEG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+    -D WITH_OPENCL=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
     -D BUILD_EXAMPLES=ON ..
 
 make -j$(nproc)
