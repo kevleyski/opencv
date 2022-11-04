a168b310b03fd215d3897b8da2ec2c79de8597ea
Kevin Staunton-Lambert <kevstal@fb.com>
2022-11-04 11:30:28 +1100

    KJSL build

--- kjsl_macos_build.sh
+++ kjsl_macos_build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 BUILD=cmake-build-debug
+HERE=$PWD
 
 mkdir -p $BUILD
 cd $BUILD || exit 1
 
+zlib () {
+  cd $HERE/.. || exit 1
+  git clone git@github.com:kevleyski/zlib.git
+  cd zlib
+  ./configure
+  make -j8
+  sudo make install
+}
+
+cd $HERE || exit 1
+
 export LDFLAGS="-L/usr/local/opt/libffi/lib"
 export CPPFLAGS="-I/usr/local/opt/libffi/include"
 
-PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH" cmake -D CMAKE_BUILD_TYPE=DEBUG \
+PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH" cmake -D CMAKE_BUILD_TYPE=DEBUG \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_C_EXAMPLES=ON \
     -D INSTALL_PYTHON_EXAMPLES=ON \
